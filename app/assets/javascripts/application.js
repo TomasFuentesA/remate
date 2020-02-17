c487a03c6f57a7b1da20bdd3b90ad6f545b9a7c3
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2020-02-17 13:21:04 -0300

    delete underscode to aplication.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
-//= require underscore/underscore
+
 //= require gmaps/google
 //= require moment/moment
 
