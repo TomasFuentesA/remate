3656ed2f3fc577e7fbdf919ebfa2e01d42f4e162
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2020-02-18 17:08:13 -0300

    fix 0.0.1

---
+++ db/migrate/20200218200343_create_table.rb
@@ -0,0 +1,6 @@
+class CreateTable < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :natural_people
+
+  end
+end
---
+++ db/migrate/20200218200715_new_table.rb
@@ -0,0 +1,22 @@
+class NewTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :natural_people do |t|
+      t.string "rut"
+      t.string "name"
+      t.string "apellido_pat"
+      t.string "apellido_mat"
+      t.integer "phone_id"
+      t.integer "mail_id"
+      t.integer "direction_id"
+      t.string "photo"
+      t.string "e_civil"
+      t.string "profesion"
+      t.date "fecha_nac"
+      t.string "alias"
+      t.string "nacionality"
+      t.string "passport"
+      t.integer "job_id"
+      t.timestamps
+    end
+  end
+end
