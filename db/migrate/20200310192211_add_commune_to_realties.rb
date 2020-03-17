2f458527b9143dce51c4202972d59d4ab55ad1e6
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2020-03-17 11:23:41 -0300

    fix

--- db/migrate/20200310192211_add_commune_to_realties.rb
+++ db/migrate/20200310192211_add_commune_to_realties.rb
@@ -1,8 +1,5 @@
 class AddCommuneToRealties < ActiveRecord::Migration[5.2]
   def change
-    remove_column :realties, :province_id, :integer
-    remove_column :realties, :region_id, :integer
-    remove_column :realties, :commune_id, :integer
     add_reference :realties, :commune, foreign_key: true
   end
 end
