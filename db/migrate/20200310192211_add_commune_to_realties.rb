a9d46be77fe28cda7863a330787404de05b9ec15
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2020-03-17 11:29:58 -0300

    fix2

--- db/migrate/20200310192211_add_commune_to_realties.rb
+++
@@ -1,5 +0,0 @@
-class AddCommuneToRealties < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :realties, :commune, foreign_key: true
-  end
-end
--- db/migrate/20200316172945_remove_index.rb
+++
@@ -1,6 +0,0 @@
-class RemoveIndex < ActiveRecord::Migration[5.2]
-  def change
-    remove_foreign_key :judgements,:auctions
-    remove_index :auctions, :judgement_id
-  end
-end
