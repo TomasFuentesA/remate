f88e3c225b02e4da6f3644a6724be64ec280c424
esarayaj <esteban.araya2@walmart.com>
2020-09-27 17:45:00 -0300

    refactor commune forms

--- app/controllers/communes_controller.rb
+++ app/controllers/communes_controller.rb
@@ -1,6 +1,8 @@
 class CommunesController < ApplicationController
   
   load_and_authorize_resource
+  before_action :set_commune, only: [:edit,:show,:destroy]
+  before_action :display_values, only: [:show]
 
   def index
     @communes = Commune.order("name").page(params[:page]).per_page(10)
@@ -34,6 +36,16 @@ class CommunesController < ApplicationController
   end
 
   private
+  def display_values
+    @able = @commune
+    @name = @able.name
+    @province_id = @able.province_id
+  end
+
+  def set_commune
+    @commune = Commune.find(params[:id])
+
+  end
 
   def commune_params
     params.require(:commune).permit(:name, :province_id )
