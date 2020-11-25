74ce5acf9db6f879f0f218618226304a8ddeeb9e
Tomas <tomas.fuentes@mail.udp.cl>
2020-11-25 14:54:44 -0300

    Rubro fix

--- app/controllers/rubros_controller.rb
+++ app/controllers/rubros_controller.rb
@@ -1,11 +1,23 @@
 class RubrosController < InheritedResources::Base
 
+  def new
+    @rubro = Rubro.new()
+  end
+
   def create
-    @legal_persona = LegalPersona.find(params[:legal_persona_id].to_i)
-    @rubro = Rubro.new(rubro_params)
-    @rubro.save
-    flash[:notice] = "Rubro añadido con exito"
-    redirect_to new_legal_persona_activity_path(@legal_persona)
+    if params[:rubro]
+      @rubro = Rubro.new(nombre: params[:rubro][:nombre])
+      @rubro.save
+      flash[:notice] = "Rubro añadido con exito"
+      redirect_to rubros_path
+    else 
+      @legal_persona = LegalPersona.find(params[:legal_persona_id].to_i)
+      @rubro = Rubro.new(rubro_params)
+      @rubro.save
+      flash[:notice] = "Rubro añadido con exito"
+      redirect_to new_legal_persona_activity_path(@legal_persona) 
+    end
+    
   end
 
   private
